3871d14efe6b5542b6d8e0188153a22d15f7c7d0
CreatorDavidZarza <zarzadavid81@gmail.com>
2026-07-07 22:21:56 -0300

    Se realizo cambio en realizar pedido, porque no respondia bien al querer agregar otro pedido

--- Realizar_pedidos.py
+++ Realizar_pedidos.py
@@ -12,124 +12,129 @@ def realizar_pedido():
         print("==============================")
         print("Número de pedido:", numero_pedido)
 
-                fileobj = open("productos.txt", "r")
-                id_producto = fileobj.readline().strip()
+        fileobj = open("productos.txt", "r")
+        id_producto = fileobj.readline().strip()
 
-                while id_producto != "":
-                    nombre_producto = fileobj.readline().strip()
-                    precio_producto = fileobj.readline().strip()
-                    stock_producto = fileobj.readline().strip()
+        while id_producto != "":
+            nombre_producto = fileobj.readline().strip()
+            precio_producto = fileobj.readline().strip()
+            stock_producto = fileobj.readline().strip()
 
-                    print("ID:", id_producto)
-                    print("Producto:", nombre_producto)
-                    print("Precio: $", precio_producto)
-                    print("Stock:", stock_producto)
-                    print("---------------------------")
+            print("ID:", id_producto)
+            print("Producto:", nombre_producto)
+            print("Precio: $", precio_producto)
+            print("Stock:", stock_producto)
+            print("---------------------------")
 
-                    id_producto = fileobj.readline().strip()
+            id_producto = fileobj.readline().strip()
 
+        fileobj.close()
+
+        id_buscado = pedir_entero("\nIngrese el ID del producto: ")
+
+        fileobj = open("productos.txt", "r")
+
+        encontrado = False
+
+        id_producto = fileobj.readline().strip()
+
+        while id_producto != "":
+
+            nombre_producto = fileobj.readline().strip()
+            precio_producto = fileobj.readline().strip()
+            stock_producto = fileobj.readline().strip()
+
+            if int(id_producto) == id_buscado:
+
+                encontrado = True
+
+                print("\nProducto seleccionado:")
+                print("ID:", id_producto)
+                print("Producto:", nombre_producto)
+                print("Precio: $", precio_producto)
+                print("Stock:", stock_producto)
+
+                cantidad = pedir_entero("\nIngrese la cantidad: ")
+
+                while cantidad > int(stock_producto):
+                    print("\nStock insuficiente.")
+                    cantidad = pedir_entero("Ingrese otra cantidad: ")
+
+                subtotal = cantidad * int(precio_producto)
+                total_pedido = total_pedido + subtotal
+
+                print("\nCantidad:", cantidad)
+                print("Subtotal: $", subtotal)
+
+                # Guardar pedido
+                pedido = open("pedidos.txt", "a")
+
+                pedido.write(str(numero_pedido) + "\n")
+                pedido.write(str(id_producto) + "\n")
+                pedido.write(str(cantidad) + "\n")
+                pedido.write(str(precio_producto) + "\n")
+                pedido.write(str(subtotal) + "\n")
+
+                pedido.close()
                 fileobj.close()
+                print("\nPedido realizado con éxito.")
 
-                id_buscado = pedir_entero("\nIngrese el ID del producto: ")
+                # Actualizar stock
+                archivo = open("productos.txt", "r")
+                auxiliar = open("auxiliar.txt", "w")
 
-                fileobj = open("productos.txt", "r")
+                id_actual = archivo.readline().strip()
 
-                encontrado = False
+                while id_actual != "":
 
-                id_producto = fileobj.readline().strip()
+                    nombre = archivo.readline().strip()
+                    precio = archivo.readline().strip()
+                    stock = archivo.readline().strip()
 
-                while id_producto != "":
+                    if int(id_actual) == id_buscado:
 
-                    nombre_producto = fileobj.readline().strip()
-                    precio_producto = fileobj.readline().strip()
-                    stock_producto = fileobj.readline().strip()
+                        nuevo_stock = int(stock) - cantidad
 
-                    if int(id_producto) == id_buscado:
+                        auxiliar.write(id_actual + "\n")
+                        auxiliar.write(nombre + "\n")
+                        auxiliar.write(precio + "\n")
+                        auxiliar.write(str(nuevo_stock) + "\n")
+                    else:
 
-                        encontrado = True
+                        auxiliar.write(id_actual + "\n")
+                        auxiliar.write(nombre + "\n")
+                        auxiliar.write(precio + "\n")
+                        auxiliar.write(stock + "\n")
 
-                        print("\nProducto seleccionado:")
-                        print("ID:", id_producto)
-                        print("Producto:", nombre_producto)
-                        print("Precio: $", precio_producto)
-                        print("Stock:", stock_producto)
+                    id_actual = archivo.readline().strip()
 
-                        cantidad = pedir_entero("\nIngrese la cantidad: ")
+                archivo.close()
+                auxiliar.close()
+               
+                remove("productos.txt")
+                rename("auxiliar.txt", "productos.txt")
 
-                        while cantidad > int(stock_producto):
-                            print("\nStock insuficiente.")
-                            cantidad = pedir_entero("Ingrese otra cantidad: ")
+                print("\nStock actualizado correctamente.")
 
-                        subtotal = cantidad * int(precio_producto)
-                        total_pedido = total_pedido + subtotal
-
-                        print("\nCantidad:", cantidad)
-                        print("Subtotal: $", subtotal)
-
-                        # Guardar pedido
-                        pedido = open("pedidos.txt", "a")
-
-                        pedido.write(str(numero_pedido) + "\n")
-                        pedido.write(str(id_producto) + "\n")
-                        pedido.write(str(cantidad) + "\n")
-                        pedido.write(str(precio_producto) + "\n")
-                        pedido.write(str(subtotal) + "\n")
-
-                        pedido.close()
-
-                        print("\nPedido realizado con éxito.")
-
-                        # Actualizar stock
-                        archivo = open("productos.txt", "r")
-                        auxiliar = open("auxiliar.txt", "w")
-
-                        id_actual = archivo.readline().strip()
-
-                        while id_actual != "":
-
-                            nombre = archivo.readline().strip()
-                            precio = archivo.readline().strip()
-                            stock = archivo.readline().strip()
-
-                            if int(id_actual) == id_buscado:
-
-                                nuevo_stock = int(stock) - cantidad
-
-                                auxiliar.write(id_actual + "\n")
-                                auxiliar.write(nombre + "\n")
-                                auxiliar.write(precio + "\n")
-                                auxiliar.write(str(nuevo_stock) + "\n")
-
-                            else:
-
-                                auxiliar.write(id_actual + "\n")
-                                auxiliar.write(nombre + "\n")
-                                auxiliar.write(precio + "\n")
-                                auxiliar.write(stock + "\n")
-
-                            id_actual = archivo.readline().strip()
-
-                        archivo.close()
-                        auxiliar.close()
-
-                        remove("productos.txt")
-                        rename("auxiliar.txt", "productos.txt")
-
-                        print("\nStock actualizado correctamente.")
-                        print("desea agregar otro producto? (1 - Sí, 2 - No)")
-                        continuar = pedir_entero("Ingrese su opción: ")
-                        while continuar != 1 and continuar != 2:
-                            continuar = pedir_entero("Ingrese su opción: ")
+                print("desea agregar otro producto? (1 - Sí, 2 - No)")
+                continuar = pedir_entero("Ingrese su opción: ")
+                while continuar != 1 and continuar != 2:
+                  continuar = pedir_entero("Ingrese su opción: ")
                             
+                break
+            id_producto = fileobj.readline().strip()
+        fileobj.close()
 
-                        break
+        if encontrado == False:
+         print("\nNo existe un producto con ese ID.")
+         continue
 
-                    id_producto = fileobj.readline().strip()
-
-                fileobj.close()
-
-                if encontrado == False:
-                    print("\nNo existe un producto con ese ID.")
+    print("\n==============================")
+    print("PEDIDO FINALIZADO")
+    print("==============================")
+    print("Número de pedido:", numero_pedido)
+    print("Total del pedido: $", total_pedido)
+    input("\nPresione ENTER para volver al menú...")
 
 
 def obtener_numero_pedido():
